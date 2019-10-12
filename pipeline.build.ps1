4567b8b5b238fc92c51aeefe5bba210a832ac963
Bernie White <bewhite@microsoft.com>
2019-10-12 18:20:43 +1000

    Fix docker build failure in CI

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -2,7 +2,10 @@
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $False)]
-    [String]$Build = '0.0.1',
+    [String]$Build = $Env:BUILD_BUILDNUMBER,
+
+    [Parameter(Mandatory = $False)]
+    [String]$BaseImage = $Env:BASEIMAGE,
 
     [Parameter(Mandatory = $True)]
     [String]$Image,
@@ -14,15 +17,11 @@ param (
     [String]$Configuration = 'Debug',
 
     [Parameter(Mandatory = $False)]
-    [String]$Registry,
-
-    [Parameter(Mandatory = $False)]
-    [String]$ArtifactPath = (Join-Path -Path $PWD -ChildPath out/modules)
+    [String]$Registry = $Env:REGISTRY
 )
 
 Write-Host -Object "[Pipeline] -- PWD: $PWD" -ForegroundColor Green;
-Write-Host -Object "[Pipeline] -- ArtifactPath: $ArtifactPath" -ForegroundColor Green;
-Write-Host -Object "[Pipeline] -- BuildNumber: $($Env:BUILD_BUILDNUMBER)" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Build: $Build" -ForegroundColor Green;
 Write-Host -Object "[Pipeline] -- SourceBranch: $($Env:BUILD_SOURCEBRANCH)" -ForegroundColor Green;
 Write-Host -Object "[Pipeline] -- SourceBranchName: $($Env:BUILD_SOURCEBRANCHNAME)" -ForegroundColor Green;
 Write-Host -Object "[Pipeline] -- Commit: $($Env:BUILD_SOURCEVERSION)" -ForegroundColor Green;
@@ -31,32 +30,10 @@ if ($Env:SYSTEM_DEBUG -eq 'true') {
     $VerbosePreference = 'Continue';
 }
 
-if ($Env:BUILD_SOURCEBRANCH -like '*/tags/*' -and $Env:BUILD_SOURCEBRANCHNAME -like 'v0.*') {
-    $Build = $Env:BUILD_SOURCEBRANCHNAME.Substring(1);
-}
-
-$version = $Build;
-$versionSuffix = [String]::Empty;
-
-if ($version -like '*-*') {
-    [String[]]$versionParts = $version.Split('-', [System.StringSplitOptions]::RemoveEmptyEntries);
-    $version = $versionParts[0];
-
-    if ($versionParts.Length -eq 2) {
-        $versionSuffix = $versionParts[1];
-    }
-}
-
-Write-Host -Object "[Pipeline] -- Using version: $version" -ForegroundColor Green;
-Write-Host -Object "[Pipeline] -- Using versionSuffix: $versionSuffix" -ForegroundColor Green;
-
-$containerRegistry = $Registry;
-
-Write-Host -Object "[Pipeline] -- Using registry: $containerRegistry" -ForegroundColor Green;
-
-$baseImage = $Env:BASEIMAGE;
-
-Write-Host -Object "[Pipeline] -- Using base image: $baseImage" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Using Registry: $Registry" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Using BaseImage: $BaseImage" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Using Image: $Image" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Using Module: $Module" -ForegroundColor Green;
 
 # Synopsis: Install NuGet provider
 task NuGet {
@@ -67,12 +44,12 @@ task NuGet {
 
 # Synopsis: Get the latest stable version
 task GetLatest NuGet, {
-    Install-Module -Name $Module -Repository PSGallery -MinimumVersion 0.10.0 -Scope CurrentUser -Force;
+    Install-Module -Name $Module -Repository PSGallery -Scope CurrentUser -Force;
 }
 
 # Synopsis: Get the latest pre-release version
 task GetPrerelease NuGet, {
-    Install-Module -Name $Module -Repository PSGallery -MinimumVersion 0.10.0 -AllowPrerelease -Scope CurrentUser -Force;
+    Install-Module -Name $Module -Repository PSGallery -AllowPrerelease -Scope CurrentUser -Force;
 }
 
 task BuildImage GetLatest, {
@@ -80,21 +57,32 @@ task BuildImage GetLatest, {
     $stableVersion = @($versions | Where-Object -FilterScript {
         $_.Version -notlike "*-*"
     })[0].Version;
+    $major = ([version]$stableVersion).Major
+    $minor = ([version]$stableVersion).Minor
     # $latestVersion = @($versions | Where-Object -FilterScript {
     #     $_.Version -like "*-*"
     # })[0].Version;
 
+    $dockerFile = "docker/$Image/stable/$BaseImage/docker/Dockerfile";
+    $buildTag = "$($Image):stable-$BaseImage";
+    $targetLatest = "$Registry/$($Image):latest-$BaseImage";
+    $targetMajor = "$Registry/$($Image):v$major-$BaseImage";
+    $targetMinor = "$Registry/$($Image):v$major.$minor-$BaseImage";
+
     exec {
-        docker build -f docker/$Image/stable/$baseImage/docker/Dockerfile -t $($Image):stable-$baseimage --build-arg VCS_REF=$Env:BUILD_SOURCEVERSION --build-arg MODULE_VERSION=$stableVersion .
-        docker tag $($Image):stable-$baseimage $containerRegistry/$($Image):latest-$baseimage
-        docker tag $($Image):stable-$baseimage $containerRegistry/$($Image):v$major-$baseimage
-        docker tag $($Image):stable-$baseimage $containerRegistry/$($Image):v$major.$minor-$baseimage
+        docker build -f $dockerFile -t $buildTag --build-arg VCS_REF=$Env:BUILD_SOURCEVERSION --build-arg MODULE_VERSION=$stableVersion .
     }
 
     exec {
-        docker push $containerRegistry/$($Image):latest-$baseImage
-        docker push $containerRegistry/$($Image):v$major-$baseimage
-        docker push $containerRegistry/$($Image):v$major.$minor-$baseimage
+        docker tag $buildTag $targetLatest
+        docker tag $buildTag $targetMajor
+        docker tag $buildTag $targetMinor
+    }
+
+    exec {
+        docker push $targetLatest
+        docker push $targetMajor
+        docker push $targetMinor
     }
 }
 
